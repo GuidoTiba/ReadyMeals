b14ae2a83214f941bd41fa62a21991514f94c73a
Guido Tibaudin <guidotibaudin@gmail.com>
2021-02-13 12:30:08 -0300

    Added validations to every model required

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,9 @@ class Event < ApplicationRecord
   has_many :event_options
   has_many :options, through: :event_options
   has_many :meals
+
+  ## Validations
+
+  validates :title, presence: true
+  validates :number_of_members, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- app/models/option.rb
+++ app/models/option.rb
@@ -3,4 +3,8 @@ class Option < ApplicationRecord
   has_many :recipe_options
   has_many :events, through: :event_options
   has_many :recipes, through: :recipe_options
+
+  ## Validations
+
+  validates :name, presence: true
 end

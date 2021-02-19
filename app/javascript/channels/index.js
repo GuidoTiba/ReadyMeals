f3e67405529181632835fc731fb23db965bdded1
Claudio Tome <cptome@gmail.com>
2021-02-18 21:50:29 -0300

    conflictos de js

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -4,11 +4,3 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
- const restrictions = document.querySelectorAll(".clickable");
-
- restrictions.forEach((restriction) => {
-   restriction.addEventListener("click", (event) => {
-     event.currentTarget.classList.toggle("active");
-     console.log(event);
-  });
- });

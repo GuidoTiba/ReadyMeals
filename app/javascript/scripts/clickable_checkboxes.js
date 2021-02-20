7365cf5fe48a5cc056f37b0dc20441293495beba
Claudio Tome <cptome@gmail.com>
2021-02-20 13:38:01 -0300

    boton diseño arreglado

--- app/javascript/scripts/clickable_checkboxes.js
+++ app/javascript/scripts/clickable_checkboxes.js
@@ -1,44 +1,16 @@
-/* const restrictions = document.querySelectorAll(".clickable");
+const restrictions = document.querySelectorAll(".oval.clickable");
 if (restrictions) {
   restrictions.forEach((restriction) => {
-   restriction.addEventListener("click", (event) => {
-     event.currentTarget.classList.toggle("active");
-     const actives = document.querySelectorAll(".active");
-     console.log(actives);
-
-     actives.forEach((element) => {console.log(`"options_${element.innerText}"`)
-      const list = document.getElementById(`"options_vegana"`);
-      const list2 = document.getElementById(`"options_mexicana"`);
-      list.value = true;
-      list2.value = true });
-   
+    restriction.addEventListener("click", (event) => {
+      event.currentTarget.classList.toggle("active");
     });
-  
   });
 }
 
+/*      const actives = document.querySelectorAll(".active");
+     console.log(actives);
 
-
-//
-//if (actives) {
-//  actives.forEach((active) => {
-//   active.addEventListener("click", (event) => {
-//     event.currentTarget.classList.value("true");
-//     active.forEach(element => console.log(element.classList));
-
- //    active.forEach(element => console.log(element.innerHTML));
- //   });
-  
- // });
-//}
-
-
-
-restrictions.forEach(element => console.log(element.classList));
-
-restrictions.forEach(element => console.log(element.innerHTML));
-
-// poner valor true a 1 elemento
-// const list = document.getElementById("options_mexicana");
-// list.value = true;
- */
+     actives.forEach((element) => {console.log(`"options_${element.innerText}"`)
+      const list2 = document.getElementById(`"options_mexicana"`);
+      list.value = true; */
+  /*   }); */

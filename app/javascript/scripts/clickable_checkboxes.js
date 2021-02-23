60067fd7f1adac3babc3eff3bf5dfc183c94cfca
Claudio Tome <cptome@gmail.com>
2021-02-23 20:30:15 -0300

    new reparado clickable checkboxes como importacion

--- app/javascript/scripts/clickable_checkboxes.js
+++ app/javascript/scripts/clickable_checkboxes.js
@@ -1,11 +1,15 @@
-const restrictions = document.querySelectorAll(".oval.clickable");
-if (restrictions) {
-  restrictions.forEach((restriction) => {
-    restriction.addEventListener("click", (event) => {
-      event.currentTarget.classList.toggle("active");
+const init_clickable_checkboxes = () => { 
+  const restrictions = document.querySelectorAll(".oval.clickable");
+  if (restrictions) {
+    restrictions.forEach((restriction) => {
+      restriction.addEventListener("click", (event) => {
+        event.currentTarget.classList.toggle("active");
+      });
     });
-  });
-}
+  }
+};
+
+export {init_clickable_checkboxes}
 
 /*      const actives = document.querySelectorAll(".active");
      console.log(actives);

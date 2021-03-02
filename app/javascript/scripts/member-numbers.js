0094a1e70e84e19c5d0042cd3aeabc9ac817ccf5
Guido Tibaudin <guidotibaudin@gmail.com>
2021-03-02 20:31:15 -0300

    Fixed numbers behaviour and now flash alerts specifies the recipe name

--- app/javascript/scripts/member-numbers.js
+++ app/javascript/scripts/member-numbers.js
@@ -1,53 +1,102 @@
 const init_member_numbers = () => { 
+  const every_number = document.querySelectorAll(".number-box");
   const member_number_input = document.querySelector("#event_number_of_members");
   if (member_number_input) {
     document.querySelector("#number-1").addEventListener("click", (event) => {
       member_number_input.value = event.target.innerText;
-      console.log(event.target)
+      every_number.forEach(number_box => {
+        if (number_box != event.target) {
+          number_box.classList.remove("active");
+        }
+      });
     });
     document.querySelector("#number-2").addEventListener("click", (event) => {
       member_number_input.value = event.target.innerText;
-      console.log(event.target)
+      every_number.forEach(number_box => {
+        if (number_box != event.target) {
+          number_box.classList.remove("active");
+        }
+      });
     });
     document.querySelector("#number-3").addEventListener("click", (event) => {
       member_number_input.value = event.target.innerText;
-      console.log(event.target)
+      every_number.forEach(number_box => {
+        if (number_box != event.target) {
+          number_box.classList.remove("active");
+        }
+      });
     });
     document.querySelector("#number-4").addEventListener("click", (event) => {
       member_number_input.value = event.target.innerText;
-      console.log(event.target)
+      every_number.forEach(number_box => {
+        if (number_box != event.target) {
+          number_box.classList.remove("active");
+        }
+      });
     });
     document.querySelector("#number-5").addEventListener("click", (event) => {
       member_number_input.value = event.target.innerText;
-      console.log(event.target)
+      every_number.forEach(number_box => {
+        if (number_box != event.target) {
+          number_box.classList.remove("active");
+        }
+      });
     });
     document.querySelector("#number-6").addEventListener("click", (event) => {
       member_number_input.value = event.target.innerText;
-      console.log(event.target)
+      every_number.forEach(number_box => {
+        if (number_box != event.target) {
+          number_box.classList.remove("active");
+        }
+      });
     });
     document.querySelector("#number-7").addEventListener("click", (event) => {
       member_number_input.value = event.target.innerText;
-      console.log(event.target)
+      every_number.forEach(number_box => {
+        if (number_box != event.target) {
+          number_box.classList.remove("active");
+        }
+      });
     });
     document.querySelector("#number-8").addEventListener("click", (event) => {
       member_number_input.value = event.target.innerText;
-      console.log(event.target)
+      every_number.forEach(number_box => {
+        if (number_box != event.target) {
+          number_box.classList.remove("active");
+        }
+      });
     });
     document.querySelector("#number-9").addEventListener("click", (event) => {
       member_number_input.value = event.target.innerText;
-      console.log(event.target)
+      every_number.forEach(number_box => {
+        if (number_box != event.target) {
+          number_box.classList.remove("active");
+        }
+      });
     });
     document.querySelector("#number-10").addEventListener("click", (event) => {
       member_number_input.value = event.target.innerText;
-      console.log(event.target)
+      every_number.forEach(number_box => {
+        if (number_box != event.target) {
+          number_box.classList.remove("active");
+        }
+      });
     });
     document.querySelector("#number-11").addEventListener("click", (event) => {
       member_number_input.value = event.target.innerText;
-      console.log(event.target)
+      every_number.forEach(number_box => {
+        if (number_box != event.target) {
+          number_box.classList.remove("active");
+        }
+      });
     });
     document.querySelector("#number-12").addEventListener("click", (event) => {
       member_number_input.value = event.target.innerText;
-      console.log(event.target)
+      every_number.forEach(number_box => {
+        if (number_box != event.target) {
+          number_box.classList.remove("active");
+        }
+      });
     });
   }
 };

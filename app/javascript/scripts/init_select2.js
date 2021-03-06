00ac942153d112a3aba24f3072ae09e4ecff0ba2
Guido Tibaudin <guidotibaudin@gmail.com>
2021-03-06 13:21:15 -0300

    Select2 working

--- app/javascript/scripts/init_select2.js
+++ app/javascript/scripts/init_select2.js
@@ -2,7 +2,11 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2(); // (~ document.querySelectorAll)
+  $('.select2').select2({
+    theme: "classic", 
+    allowClear: true,
+    minimumResultsForSearch: -1
+  }); // (~ document.querySelectorAll)
 };
 
 export { initSelect2 };
